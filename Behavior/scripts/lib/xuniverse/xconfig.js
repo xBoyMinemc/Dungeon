a5f0db564cf00ff0337d584f5345a4c2bf047cd7
xboyMinemc <xboy.minemc@gmail.com>
2022-03-15 00:57:16 +0800

    BAD UP

---
+++ Behavior/scripts/lib/xuniverse/xconfig.js
@@ -0,0 +1,13 @@
+import { world } from "mojang-minecraft";
+
+
+const overworld = world.getDimension("overworld");
+const nether = world.getDimension("nether");
+const the_end = world.getDimension("the end");
+
+const orxyz = [-704,0,-160];//核心参数，地牢起始坐标
+const where = the_end;      //核心参数，地牢所在维度
+
+
+
+export { orxyz, where };
