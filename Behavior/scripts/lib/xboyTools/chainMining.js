4157860da9fb70e3c5414622bce148fcbd566abe
xBoyMinemc <86919167+xBoyMinemc@users.noreply.github.com>
2022-06-25 17:58:45 +0800

    添加木材连锁

--- Behavior/scripts/lib/xboyTools/chainMining.js
+++ Behavior/scripts/lib/xboyTools/chainMining.js
@@ -1,11 +1,12 @@
-//#=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠#//
-//#
-//#=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠#//
+//#=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=#//
+//#https://github.com/xBoyMinemc/Dungeon/blob/master/Behavior/scripts/lib/xboyTools/chainMining.js   
+//#=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=≠=#//
 //
 
 import {  BlockLocation } from "mojang-minecraft";
 
-var   List_B_Object = {}
+var   List_AA_Object = {}
+var   List_BB_Object = {}
 let mineCount = 0
 const MAX_MINE_COUNT = 64 - 1      //这个数字只是个大概，因为写的并不严谨，反正自己用的
 const List_A_Object = {
@@ -16,6 +17,13 @@ const List_A_Object = {
           "minecraft:iron_pickaxe"         : [ "minecraft:gold_ore", "minecraft:deepslate_gold_ore", "minecraft:emerald_ore", "minecraft:deepslate_emerald_ore", "minecraft:diamond_ore", "minecraft:deepslate_diamond_ore", "minecraft:lit_redstone_ore", "minecraft:deepslate_lit_redstone_ore", "minecraft:redstone_ore", "minecraft:deepslate_redstone_ore"],
           "minecraft:diamond_pickaxe"      : [ "minecraft:ancient_debris"],//,"minecraft:gold_block", "minecraft:deepslate_bricks"
           "minecraft:netherite_pickaxe"    : []
+}
+const List_B_Object = {
+          "minecraft:golden_axe"       : ["minecraft:log", "minecraft:log2", "minecraft:mangrove_log", "minecraft:crimson_stem", "minecraft:warped_stem"],//, "minecraft:", "minecraft:"
+          "minecraft:stone_axe"        : [],
+          "minecraft:iron_axe"         : [],
+          "minecraft:diamond_axe"      : [],
+          "minecraft:netherite_axe"    : []
 }
 
 var neighborBlock = function(blockLocationO, dimension, blockid){
@@ -30,7 +38,7 @@ var neighborBlock = function(blockLocationO, dimension, blockid){
                                    W : new BlockLocation(blockX - 1, blockY, blockZ),
                                    S : new BlockLocation(blockX, blockY, blockZ + 1)
                              }
-                        const point  = ["U","D","N","E","W","S"]
+                        const point  = ["U","D","N","E","W","S"]//被挖掘方块六面接触的方块
                         
                         point.forEach(p=>{
                         let bl = point_X[p]
@@ -50,39 +58,58 @@ var neighborBlock = function(blockLocationO, dimension, blockid){
 }
 //EndFunction
 
-const List_Array = Object.keys(List_A_Object)//希望别给我整幺蛾子，希望不要乱序，希望不要乱序，希望不要乱序
+const List_Array_A = Object.keys(List_A_Object)//希望别给我整幺蛾子，希望不要乱序，希望不要乱序，希望不要乱序
 
-List_Array.forEach(item => {
-List_B_Object[item] = []
-for(let i=List_Array.indexOf(item);i>-1;i--){
+List_Array_A.forEach(item => {
+List_AA_Object[item] = []
+for(let i=List_Array_A.indexOf(item);i>-1;i--){
 
-List_B_Object[item] = List_B_Object[item].concat(List_A_Object[List_Array[i]])
+List_AA_Object[item] = List_AA_Object[item].concat(List_A_Object[List_Array_A[i]])
+
+}
+})
+
+const List_Array_B = Object.keys(List_B_Object)//希望别给我整幺蛾子，希望不要乱序，希望不要乱序，希望不要乱序
+
+List_Array_B.forEach(item => {
+List_BB_Object[item] = []
+for(let i=List_Array_B.indexOf(item);i>-1;i--){
+
+List_BB_Object[item] = List_BB_Object[item].concat(List_B_Object[List_Array_B[i]])
 
 }
 })
 
 let chainMining = function(breakBlockId, block, dimension, player){
-                   //     let mineCount = 0
+                           //坐标id获取   锚点      执行者
 
-                       //坐标id获取   锚点      执行者
+                           
                      let itemObject = player.getComponent("inventory").container.getItem(player.selectedSlot)
                      let xboy;
-                     //if(itemObject){
-                     //if(itemObject !== undefined){
+                     
+
                      if(itemObject){
                      let itemIdString = itemObject.id
-                     if (List_Array.includes(itemIdString)) {
+                     if (List_Array_A.includes(itemIdString)) {
                      //#IF#1
-                        if (List_B_Object[itemIdString].includes(breakBlockId)) {
+                        if (List_AA_Object[itemIdString].includes(breakBlockId)) {
                         //#IF#2
-                     //   let blockLocationO = new BlockLocation(block.x, blockY, blockZ)
                         neighborBlock(block, dimension, breakBlockId)
-                        
-      }
-   //EndIF#2
+                        }
+                        //EndIF#2
+                     }
+                     //#EndIF#1
+
+                     if (List_Array_B.includes(itemIdString)) {
+                        //#IF#1
+                           if (List_BB_Object[itemIdString].includes(breakBlockId)) {
+                           //#IF#2
+                           neighborBlock(block, dimension, breakBlockId)
+                           }
+                           //EndIF#2
+                        }
+                        //#EndIF#1
    }
- //#EndIF#1
-}
  let i = mineCount;
  mineCount = 0;
  return i;
